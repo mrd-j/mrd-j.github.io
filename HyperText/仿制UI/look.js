a3975ab8d5dbe866ef478d485caf655792b2e233
mrd-j <547144286@qq.com>
2023-11-18 16:54:05 +0800

    ..

--- HyperText/仿制UI/look.js
+++ HyperText/仿制UI/look.js
@@ -1,25 +1,24 @@
 $(function(){
-    $("button").click(function(){
-        var id = $(this).attr("class");
+    function loadJson(name){
         $.ajax({
             type: "get",
-            url: "data/"+id+".json",
+            url: "data/"+name+".json",
             dataType: "json",
             success: function (data) {
-                $("#count").text("总计有" + data.length + "项"+id+"！");
+                $("#count").text("总计有" + data.length + "项"+name+"！");
                 var counter = 0;
                 $.each(data, function(index,item){
-                    if ($.isArray(item)) {
-                        $.each(item,function(key,value){
-                            $("#count").append("<br>"+value.名称);
-                        });
-                    }
-                    else{
                         counter = counter + 1;
                         $("#count").append("<br>" + counter + "." + item.名称);
-                    }
                 });
             }
         });
+    }
+    
+    $("#xinfa").click(function(){
+        loadJson("心法");
+    });
+    $("#skill").click(function(){
+        loadJson("技能");
     });
 });
